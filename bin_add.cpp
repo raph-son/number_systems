08df90b0d6564001bb0d94dfcdeacb446a642838
raph-son <samezzy@yahoo.com>
2019-07-17 07:39:02 +0100

    Added support for varing operand length

--- bin_add.cpp
+++ bin_add.cpp
@@ -18,6 +18,14 @@ using namespace std;
 */
 char do_add(bool& carry, char left, char right);
 
+/*
+    Prefix the operand with less bit with 0's
+
+    @param string - operands
+    @return void
+*/
+void zero_prefix(string& left_operand, string& right_operand);
+
 // Program's entry point
 int main(int c, char* argv[]) {
     if(c < 3) {
@@ -26,13 +34,19 @@ int main(int c, char* argv[]) {
     }
 
     bool carry_switch;
-    char add_begins_left = argv[1][strlen(argv[1]) - 1];
-    char add_begins_right = argv[2][strlen(argv[2]) - 1];
+    string left_operand = argv[1];
+    string right_operand = argv[2];
+
+    // Add as much zero beind the operand with less bit where needed
+    zero_prefix(left_operand, right_operand);
+
+    char add_begins_left = left_operand[left_operand.length() - 1];
+    char add_begins_right = right_operand[right_operand.length() - 1];
 
     string result;
-
-    for(int i = strlen(argv[1]); i != 0; i--) {
-        result.insert(0, 1, do_add(carry_switch, argv[1][i - 1], argv[2][i - 1]));
+    // Since both side have equal length now, we can use either side 
+    for(int i = add_begins_left; i != 0; i--) {
+        result.insert(0, 1, do_add(carry_switch, left_operand[i - 1], right_operand[i - 1]));
     }
     // If last operation has a carry, just add one the the beginning
     if(carry_switch) result.insert(0, 1, '1');
@@ -41,7 +55,24 @@ int main(int c, char* argv[]) {
     return 0;
 }
 
+// Prefix the operand with less characters with 0 to equal other length
+void zero_prefix(string& left_operand, string& right_operand) {
+    if(left_operand.length() > right_operand.length()) {
+        int zero_length_to_prefix = left_operand.length() \
+                                    - right_operand.length();
+        for(int i = zero_length_to_prefix; i != 0; i--) {
+            right_operand.insert(0, 1, '0');
+        }
+    }
 
+    if(left_operand.length() < right_operand.length()) {
+        int zero_length_to_prefix = right_operand.length() \
+                                    - left_operand.length();
+        for(int i = zero_length_to_prefix; i != 0; i--) {
+            left_operand.insert(0, 1, '0');
+        }
+    }
+}
 
 // Determin if a binary addition involve carring or not
 char do_add(bool& carry, char left, char right) {
